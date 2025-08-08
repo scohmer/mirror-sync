c981314bec68def401c0c2be19e7ebdc0a5f1305
Sean Cohmer <sean@Seans-MacBook-Air.local>
2025-08-08 19:51:48 -0400

    version corrections, in progress

--- debian-12/security.debian.org/sync-debian-security.sh
+++ debian-12/security.debian.org/sync-debian-security.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-echo "[*] Starting Debian 11 (bullseye) security sync..."
+echo "[*] Starting Debian 12 (bookworm) security sync..."
 
 debmirror /debian-mirror \
   --host=security.debian.org \
   --root=debian-security \
   --method=http \
-  --dist=bullseye-security \
+  --dist=bookworm-security \
   --section=main,contrib,non-free,non-free-firmware,updates \
   --arch=amd64,i386 \
   --i18n \
@@ -15,4 +15,4 @@ debmirror /debian-mirror \
   --ignore-missing-release \
   --keyring /usr/share/keyrings/debian-archive-keyring.gpg
 
-echo "[✓] Debian 11 security sync complete and ready for offline use."
+echo "[✓] Debian 12 security sync complete and ready for offline use."
--- debian-13/build-and-sync.sh
+++ debian-13/build-and-sync.sh
@@ -2,21 +2,39 @@
 set -e
 
 # Define names
-IMAGE_NAME=debian-mirror:bookworm
-MIRROR_DIR=/srv/debian-mirror
+DEB_IMAGE_NAME=debian-13-mirror:trixie
+SEC_IMAGE_NAME=security-13-mirror:trixie
+DEB_MIRROR_DIR=/srv/apt/debian/deb.debian.org/debian
+SEC_MIRROR_DIR=/srv/apt/debian/security.debian.org/debian-security
 
+# Processes related to Debian 11 mirror synchronization
 # Build the container image
 echo "[*] Building container image..."
-podman build -t "$IMAGE_NAME" .
+podman build -t "$DEB_IMAGE_NAME" .
 
 # Ensure mirror directory exists
-sudo mkdir -p "$MIRROR_DIR"
-sudo chown -R root:root "$MIRROR_DIR"
-sudo chcon -Rt container_file_t "$MIRROR_DIR"  # Needed for SELinux
+sudo mkdir -p "$DEB_MIRROR_DIR"
+sudo chown -R root:root "$DEB_MIRROR_DIR"
+sudo chcon -Rt container_file_t "$DEB_MIRROR_DIR"  # Needed for SELinux
 
 # Run the container to sync the mirror
 echo "[*] Running container to sync mirror..."
-podman run --rm -v "$MIRROR_DIR":/debian-mirror:Z "$IMAGE_NAME"
+podman run --rm -v "$DEB_MIRROR_DIR":/debian-mirror:Z "$DEB_IMAGE_NAME"
 
-echo "[✓] Mirror sync complete at: $MIRROR_DIR"
+echo "[✓] Debian mirror sync complete at: $DEB_MIRROR_DIR"
 
+# Processes related to Debian 11 security mirror synchronization
+# Build the container image
+echo "[*] Building container image..."
+podman build -t "$SEC_IMAGE_NAME" .
+
+# Ensure mirror directory exists
+sudo mkdir -p "$SEC_MIRROR_DIR"
+sudo chown -R root:root "$SEC_MIRROR_DIR"
+sudo chcon -Rt container_file_t "$SEC_MIRROR_DIR"  # Needed for SELinux
+
+# Run the container to sync the mirror
+echo "[*] Running container to sync mirror..."
+podman run --rm -v "$SEC_MIRROR_DIR":/debian-mirror:Z "$SEC_IMAGE_NAME""
+
+echo "[✓] Security mirror sync complete at: $SEC_MIRROR_DIR"
