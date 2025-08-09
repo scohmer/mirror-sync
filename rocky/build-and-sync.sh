6cc5f2b81ab8bc20283a99dea73412e5a0aad5b1
Sean Cohmer <sean@seancohmer.com>
2025-08-09 10:32:03 -0400

    debugging rocky, in progress 3

--- rocky/build-and-sync.sh
+++ rocky/build-and-sync.sh
@@ -1,4 +1,4 @@
 podman build -t rocky-mirror-all ./dl.rockylinux.org
-sudo mkdir -p /srv/yum
-sudo chcon -Rt container_file_t /srv/yum
-podman run --rm -v /srv/yum:/rocky-mirror:Z rocky-mirror-all
+sudo mkdir -p /srv/yum/rocky/pub
+sudo chcon -Rt container_file_t /srv/yum/rocky/pub
+podman run --rm -v /srv/yum/rocky/pub:/rocky-mirror:Z rocky-mirror-all
